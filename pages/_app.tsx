ce5816fefadd321c5d96a2abe42dccd66d552880
HuynhVo <vhuynh19@HuynhVos-MacBook-Pro.local>
2022-12-02 15:15:07 +0700

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import HomeLayout from '~/views/layouts/app-layouts/HomeLayout';
 import '@styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
+  //
   return (
     <HomeLayout>
       <Component {...pageProps} />
