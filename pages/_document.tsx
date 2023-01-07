fbacd07b054a78b18e5e26e03bd7bb8a9c172f91
vhuynh19 <vohuynh01092002@gmail.com>
2023-01-07 13:35:52 +0700

    Set up MUI

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,42 @@
-import React, { ReactElement } from "react";
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentInitialProps,
-  DocumentContext,
-} from "next/document";
+import React, { ReactElement } from 'react';
+import Document, { Html, Head, Main, NextScript, DocumentInitialProps, DocumentContext } from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
+import createEmotionCache from '@src/utils/functions/createEmotionCache';
+import { AppType } from 'next/app';
+import { EmotionCache } from '@emotion/react';
+
+type AppCustomType = {
+  emotionCache?: EmotionCache;
+};
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const originalRenderPage = ctx.renderPage;
-
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks } = createEmotionServer(cache);
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) => <App {...props} />,
+          enhanceApp: (App: AppType<AppCustomType>) => (props) => <App {...props} pageProps={{ emotionCache: cache }} />,
         });
 
       const initialProps = await Document.getInitialProps(ctx);
+      const emotionStyles = extractCriticalToChunks(initialProps.html);
+      const emotionStyleTags = emotionStyles.styles.map((style) => (
+        <style
+          data-emotion={`${style.key} ${style.ids.join(' ')}`}
+          key={style.key}
+          dangerouslySetInnerHTML={{ __html: style.css }}
+        />
+      ));
+
       return {
         ...initialProps,
-        styles: [<>{initialProps.styles}</>],
+        styles: [
+          <>
+            {initialProps.styles} {emotionStyleTags}
+          </>,
+        ],
       };
     } finally {
     }
@@ -32,7 +45,9 @@ export default class MyDocument extends Document {
   render(): ReactElement {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
         <body>
           <Main />
           <NextScript />
---
+++ views/StreamRoom/index.tsx
@@ -0,0 +1,27 @@
+import { Col, Row } from 'antd';
+
+const StreamRoom = () => {
+  return (
+    <Row>
+      <Col xs={12} sm={12} md={12}>
+        <div style={{ width: '100%', minHeight: '600px', backgroundColor: 'red' }}> </div>
+      </Col>
+
+      <Col xs={12} sm={12} md={12}>
+        <Row>
+          <Col span={24}>
+            <div style={{ width: '100%', minHeight: '300px', backgroundColor: 'blue' }}> </div>
+          </Col>
+          <Col span={12}>
+            <div style={{ width: '100%', minHeight: '300px', backgroundColor: 'green' }}> </div>
+          </Col>
+          <Col span={12}>
+            <div style={{ width: '100%', minHeight: '300px', backgroundColor: 'grey' }}> </div>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+};
+
+export default StreamRoom;
