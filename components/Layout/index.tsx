39a8d5ba9af3243ae6161a5d6b5db8ff8fb9561e
bdbao <baobuiduy.vn@gmail.com>
2023-01-07 12:27:05 +0700

    Set up ui

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,17 +1,11 @@
-import { Popover, Row, Space, type MenuProps, Col } from "antd";
-import { FC, useContext } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { Popover, Row, Space, type MenuProps, Col } from 'antd';
+import { FC, useContext } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
-import { PAGE_ROUTES } from "@src/utils/constants/routes";
-import { useLocale } from "@src/hooks/useLocale";
-import AppContext from "@src/contexts/AppContext";
-
-import useGroups from "@profile/hooks/useGroups";
-import { GroupState } from "@profile/types/states";
-
-import { Icon } from "@components";
+import { PAGE_ROUTES } from '@src/utils/constants/routes';
+import { useLocale } from '@src/hooks/useLocale';
+import AppContext from '@src/contexts/AppContext';
 
 import {
   AppLogo,
@@ -23,55 +17,34 @@ import {
   StyledLayout,
   StyledMenu,
   LocaleItem,
-} from "./styled";
+} from './styled';
 
-type MenuItem = Required<MenuProps>["items"][number];
+type MenuItem = Required<MenuProps>['items'][number];
 
 type Props = {
   children?: JSX.Element;
 };
 
 const Layout: FC<Props> = ({ children }) => {
-  const { t } = useLocale("common");
+  const { t } = useLocale('common');
   const { isDark, switchTheme, setLocaleSetting } = useContext(AppContext);
-  const { groups } = useGroups();
 
   return (
     <StyledLayout>
       <StyledHeader>
         <Link className="logo" href={PAGE_ROUTES.HOME}>
-          <Image
-            width={44}
-            height={44}
-            alt="fav"
-            src={"https://raroin.creabik.com/assets/img/logos/raroin.svg"}
-          />
+          <Image width={44} height={44} alt="fav" src={'https://raroin.creabik.com/assets/img/logos/raroin.svg'} />
           Funding Block
         </Link>
 
-        <StyledMenu
-          mode="horizontal"
-          items={itemList(t, groups)}
-          defaultOpenKeys={["board"]}
-          defaultSelectedKeys={["0"]}
-        />
+        <StyledMenu mode="horizontal" items={itemList(t)} defaultOpenKeys={['board']} defaultSelectedKeys={['0']} />
 
         <Space align="center" size="large">
           <Popover content={localePopoverContent(t, setLocaleSetting)}>
-            <HeaderIcon>
-              <Icon name="translate" size="lg" />
-            </HeaderIcon>
+            <HeaderIcon></HeaderIcon>
           </Popover>
 
-          <HeaderIcon>
-            <Icon
-              onClick={() => switchTheme(!isDark)}
-              name={isDark ? "lightMode" : "darkMode"}
-              size="lg"
-            />
-          </HeaderIcon>
-
-          <WalletMultiButton />
+          <HeaderIcon></HeaderIcon>
         </Space>
       </StyledHeader>
 
@@ -92,16 +65,8 @@ const Layout: FC<Props> = ({ children }) => {
               </Col>
             );
           })}
-          <Col
-            span={24 / (footerColumns(t).length + 1)}
-            style={{ textAlign: "center" }}
-          >
-            <Image
-              width={100}
-              height={100}
-              alt="fav"
-              src={"https://raroin.creabik.com/assets/img/logos/raroin.svg"}
-            />
+          <Col span={24 / (footerColumns(t).length + 1)} style={{ textAlign: 'center' }}>
+            <Image width={100} height={100} alt="fav" src={'https://raroin.creabik.com/assets/img/logos/raroin.svg'} />
             <Link href="/">
               <h1>Download now</h1>
             </Link>
@@ -115,26 +80,20 @@ const Layout: FC<Props> = ({ children }) => {
 const localePopoverContent = (t: any, setLocaleSetting: any) => {
   return (
     <LocaleWrapper>
-      <LocaleItem type="text" onClick={() => setLocaleSetting({ lang: "en" })}>
-        {t("english")}
+      <LocaleItem type="text" onClick={() => setLocaleSetting({ lang: 'en' })}>
+        {t('english')}
       </LocaleItem>
-      <LocaleItem type="text" onClick={() => setLocaleSetting({ lang: "vi" })}>
-        {t("vietnamese")}
+      <LocaleItem type="text" onClick={() => setLocaleSetting({ lang: 'vi' })}>
+        {t('vietnamese')}
       </LocaleItem>
     </LocaleWrapper>
   );
 };
 
-const itemList = (t: any, groups: GroupState[]): MenuItem[] => [
-  getItem("home", <Link href={PAGE_ROUTES.HOME}>{t("home")}</Link>),
-  getItem(
-    "questions",
-    <Link href={PAGE_ROUTES.QUESTIONS}>{t("questions")}</Link>
-  ),
-  getItem(
-    "createQuest",
-    <Link href={PAGE_ROUTES.CREATE_QUEST}>{t("createQuest")}</Link>
-  ),
+const itemList = (t: any): MenuItem[] => [
+  getItem('home', <Link href={PAGE_ROUTES.HOME}>{t('home')}</Link>),
+  getItem('questions', <Link href={PAGE_ROUTES.QUESTIONS}>{t('questions')}</Link>),
+  getItem('createQuest', <Link href={PAGE_ROUTES.CREATE_QUEST}>{t('createQuest')}</Link>),
 ];
 
 const getItem = (
@@ -142,7 +101,7 @@ const getItem = (
   label?: React.ReactNode,
   icon?: React.ReactNode,
   children?: MenuItem[],
-  type?: "group"
+  type?: 'group',
 ): MenuItem => {
   return {
     key,
@@ -155,57 +114,57 @@ const getItem = (
 
 const footerColumns = (t: any) => [
   {
-    title: "About Us",
+    title: 'About Us',
     children: [
       {
-        title: "About",
-        link: "/",
+        title: 'About',
+        link: '/',
       },
       {
-        title: "Careers",
-        link: "/",
+        title: 'Careers',
+        link: '/',
       },
       {
-        title: "Business Contact",
-        link: "/",
+        title: 'Business Contact',
+        link: '/',
       },
     ],
   },
   {
-    title: "Communities",
+    title: 'Communities',
     children: [
       {
-        title: "Mediums",
-        link: "/",
+        title: 'Mediums',
+        link: '/',
       },
       {
-        title: "Twitter",
-        link: "/",
+        title: 'Twitter',
+        link: '/',
       },
       {
-        title: "Facebook",
-        link: "/",
+        title: 'Facebook',
+        link: '/',
       },
       {
-        title: "Twitter",
-        link: "/",
+        title: 'Twitter',
+        link: '/',
       },
     ],
   },
   {
-    title: "Help",
+    title: 'Help',
     children: [
       {
-        title: "Bug Report",
-        link: "/",
+        title: 'Bug Report',
+        link: '/',
       },
       {
-        title: "FAQ",
-        link: "/",
+        title: 'FAQ',
+        link: '/',
       },
       {
-        title: "Discussions",
-        link: "/",
+        title: 'Discussions',
+        link: '/',
       },
     ],
   },
