64f3c57b8b1f10dd5c31f29032367faccf692210
qkt-acient8 <khoita@ancient8.gg>
2023-01-07 19:34:43 +0700

    save

--- src/utils/constants/routes.ts
+++ src/utils/constants/routes.ts
@@ -3,8 +3,10 @@ export const PAGE_ROUTES = {
   STREAM_ROOM: (id: string) => `/stream-room/${id}`,
   STREAM: '/stream',
   PROFILE: '/profile',
+  LOGIN: 'login',
+  REGISTER: 'register',
 };
 
-export const PUBLIC_ROUTES = [PAGE_ROUTES.HOME, PAGE_ROUTES.STREAM_ROOM];
+export const PUBLIC_ROUTES = [PAGE_ROUTES.HOME, PAGE_ROUTES.STREAM_ROOM, PAGE_ROUTES.LOGIN, PAGE_ROUTES.REGISTER];
 
 export const PRIVATE_ROUTES = [PAGE_ROUTES.PROFILE, PAGE_ROUTES.STREAM];
