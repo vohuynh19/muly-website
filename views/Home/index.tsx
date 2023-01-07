fc172d44663c13e638786b884019e66ba9867b67
qkt-acient8 <khoita@ancient8.gg>
2023-01-07 14:38:43 +0700

    save

--- views/Home/index.tsx
+++ views/Home/index.tsx
@@ -1,19 +1,30 @@
 import { Col, Row } from 'antd';
-import { Card } from './styled';
+import { Card, Text } from './styled';
 
 const Home = () => {
   return (
     <div>
       <Row>
-        <Col xs={24} sm={12} md={12} style={{ backgroundColor: 'red', height: '400px' }}></Col>
+        <Col xs={24} sm={12} md={12}>
+          <Card src="assets/images/video-thumbnail-1.jpeg" height={400} />
+        </Col>
         <Col xs={0} sm={0} md={12}>
           <Row>
             <Col span={24}>
-              <Card src="" />
+              <Card src="assets/images/video-thumbnail-2.jpeg" height={200} />
             </Col>
-            <Col span={12} style={{ backgroundColor: 'green', height: '200px' }}></Col>
-            <Col span={12} style={{ backgroundColor: 'black', height: '200px' }}></Col>
+            <Col span={12} style={{ backgroundColor: 'green', height: '200px' }}>
+              <Card src="assets/images/video-thumbnail-3.jpeg" height={200} />
+            </Col>
+            <Col span={12} style={{ backgroundColor: 'black', height: '200px' }}>
+              <Card src="assets/images/video-thumbnail-3.jpeg" height={200} />
+            </Col>
           </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Text></Text>
         </Col>
       </Row>
     </div>
