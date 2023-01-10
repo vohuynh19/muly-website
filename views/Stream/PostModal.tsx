84fc1889c318f89bb10b8989a70b95d4e1a5a937
bdbao <baobuiduy.vn@gmail.com>
2023-01-11 02:33:46 +0700

    Done stream

---
+++ views/Stream/PostModal.tsx
@@ -0,0 +1,100 @@
+import { Input, Modal, Upload, message } from 'antd';
+import { forwardRef, ForwardRefRenderFunction, useImperativeHandle, useState } from 'react';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import type { UploadChangeParam } from 'antd/es/upload';
+import type { RcFile, UploadFile, UploadProps } from 'antd/es/upload/interface';
+import { StyledForm } from './styled';
+
+type Props = {};
+export type ModelHandler = {
+  toggle: () => void;
+};
+
+const { Item, useForm } = StyledForm;
+
+const getBase64 = (img: RcFile, callback: (url: string) => void) => {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result as string));
+  reader.readAsDataURL(img);
+};
+
+const beforeUpload = (file: RcFile) => {
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+  if (!isJpgOrPng) {
+    message.error('You can only upload JPG/PNG file!');
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error('Image must smaller than 2MB!');
+  }
+  return isJpgOrPng && isLt2M;
+};
+
+const PostModal: ForwardRefRenderFunction<ModelHandler, Props> = ({}, ref) => {
+  const [form] = useForm();
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState<string>();
+
+  const [open, setOpen] = useState(false);
+  useImperativeHandle(ref, () => ({
+    toggle: () => setOpen(!open),
+  }));
+
+  const handleChange: UploadProps['onChange'] = (info: UploadChangeParam<UploadFile>) => {
+    if (info.file.status === 'uploading') {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj as RcFile, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+      });
+    }
+  };
+
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
+
+  return (
+    <Modal open={open} onCancel={() => setOpen(false)}>
+      <StyledForm form={form} layout="vertical">
+        <Item label={'Title'}>
+          <Input placeholder="Enter title" size="large" />
+        </Item>
+        <Item label={'Description'}>
+          <Input placeholder="Enter Description" size="large" />
+        </Item>
+
+        <Item label={'Preview Image'}>
+          <Upload
+            name="avatar"
+            listType="picture-card"
+            className="avatar-uploader"
+            showUploadList={false}
+            action={(file) => {
+              const reader = new FileReader();
+              reader.readAsBinaryString(file);
+              reader.onloadend = function () {
+                const base64Image = btoa(this.result as string);
+              };
+
+              return '';
+            }}
+            beforeUpload={beforeUpload}
+            onChange={handleChange}
+          >
+            {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+          </Upload>
+        </Item>
+      </StyledForm>
+    </Modal>
+  );
+};
+
+export default forwardRef(PostModal);
--- views/Stream/index.tsx
+++ views/Stream/index.tsx
@@ -1,165 +1,97 @@
-import React, { useEffect, useRef, useState } from 'react';
-import io from 'socket.io-client';
-import Peer from 'simple-peer';
-import styled from 'styled-components';
+import { useEffect, useRef, useState } from 'react';
+import { Button } from 'antd';
+import ScreenShareIcon from '@mui/icons-material/ScreenShare';
+import CameraIcon from '@mui/icons-material/Camera';
 
-const pc_config = {
-  iceServers: [
-    {
-      urls: 'stun:relay.metered.ca:80',
-    },
-    {
-      urls: 'turn:relay.metered.ca:80',
-      username: 'c68320f96ec907f1df6af2d1',
-      credential: '8ishceLvdUrTBe2n',
-    },
-    {
-      urls: 'turn:relay.metered.ca:443',
-      username: 'c68320f96ec907f1df6af2d1',
-      credential: '8ishceLvdUrTBe2n',
-    },
-    {
-      urls: 'turn:relay.metered.ca:443?transport=tcp',
-      username: 'c68320f96ec907f1df6af2d1',
-      credential: '8ishceLvdUrTBe2n',
-    },
-  ],
-};
+import { Wrapper, ScreenView, ToolBar, ToolbarButton, ScreenWrapper } from './styled';
+import PostModal, { ModelHandler } from './PostModal';
 
-const Container = styled.div`
-  padding: 20px;
-  display: flex;
-  height: 100vh;
-  width: 90%;
-  margin: auto;
-  flex-wrap: wrap;
-`;
+const Stream = () => {
+  const [isShareScreen, setIsShareScreen] = useState(false);
+  const [isTurnOnCamera, setIsTurnOnCamera] = useState(false);
 
-const StyledVideo = styled.video`
-  height: 40%;
-  width: 50%;
-`;
+  const cameraRef = useRef<HTMLVideoElement | null>(null);
+  const displayRef = useRef<HTMLVideoElement | null>(null);
 
-const Video = (props: any) => {
-  const ref = useRef<any>();
+  const streamCameraRef = useRef<MediaStream | null>(null);
+  const streamDisplayRef = useRef<MediaStream | null>(null);
+
+  const modelRef = useRef<ModelHandler>(null);
+
+  const turnCamera = () => {
+    if (isTurnOnCamera) {
+      setIsTurnOnCamera(false);
+      disconnectCamera();
+      return;
+    }
+
+    navigator.mediaDevices.getUserMedia({ video: { width: 240, height: 240 } }).then((value) => {
+      setIsTurnOnCamera(true);
+      cameraRef.current!.srcObject = value;
+      streamCameraRef.current = value;
+    });
+  };
+
+  const shareScreen = () => {
+    if (isShareScreen) {
+      setIsShareScreen(false);
+      disconnectDisplay();
+      return;
+    }
+
+    navigator.mediaDevices.getDisplayMedia().then((value) => {
+      displayRef.current!.srcObject = value;
+      streamDisplayRef.current = value;
+
+      setIsShareScreen(true);
+    });
+  };
+
+  const disconnectCamera = () => {
+    streamCameraRef.current?.getTracks()?.forEach((track) => {
+      track.stop();
+    });
+  };
+
+  const disconnectDisplay = () => {
+    streamDisplayRef.current?.getTracks()?.forEach((track) => {
+      track.stop();
+    });
+  };
 
   useEffect(() => {
-    props.peer.on('stream', (stream: any) => {
-      ref.current.srcObject = stream;
-    });
+    return () => {
+      disconnectCamera();
+      disconnectDisplay();
+    };
   }, []);
-
-  return <StyledVideo playsInline autoPlay ref={ref} />;
-};
-
-const SOCKET_SERVER_URL = '18.144.54.166:8000';
-
-const videoConstraints = {
-  height: window.innerHeight / 2,
-  width: window.innerWidth / 2,
-};
-
-const Room = (props: any) => {
-  const [peers, setPeers] = useState<any>([]);
-  const socketRef = useRef<any>();
-  const userVideo = useRef<any>();
-  const peersRef = useRef<any>([]);
-  const roomID = '1';
-
-  useEffect(() => {
-    socketRef.current = io(SOCKET_SERVER_URL, {
-      transports: ['websocket'],
-    });
-
-    navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }).then((stream) => {
-      userVideo.current.srcObject = stream;
-      socketRef.current.emit('join room', roomID);
-
-      console.log('all users');
-
-      socketRef.current.on('all users', (users: any) => {
-        const peers: any = [];
-        users.forEach((userID: any) => {
-          const peer = createPeer(userID, socketRef.current.id, stream);
-
-          peersRef.current.push({
-            peerID: userID,
-            peer,
-          });
-
-          peers.push(peer);
-        });
-        setPeers(peers);
-      });
-
-      // Kết nối với các peer có sẳn
-      socketRef.current.on('user joined', (payload: any) => {
-        console.log('user joined');
-
-        const peer = addPeer(payload.signal, payload.callerID, stream);
-        peersRef.current.push({
-          peerID: payload.callerID,
-          peer,
-        });
-
-        setPeers((users: any) => [...users, peer]);
-      });
-
-      socketRef.current.on('receiving returned signal', (payload: any) => {
-        const item = peersRef.current.find((p: any) => p.peerID === payload.id);
-        item.peer.signal(payload.signal);
-      });
-    });
-  }, []);
-
-  function createPeer(userToSignal: any, callerID: any, stream: any) {
-    const peer = new Peer({
-      initiator: true,
-      trickle: false,
-      stream,
-      config: pc_config,
-    });
-
-    peer.on('signal', (signal) => {
-      console.log('sending signal');
-
-      socketRef.current.emit('sending signal', {
-        userToSignal,
-        callerID,
-        signal,
-      });
-    });
-
-    return peer;
-  }
-
-  function addPeer(incomingSignal: any, callerID: any, stream: any) {
-    console.log('addPeer');
-
-    const peer = new Peer({
-      initiator: false,
-      trickle: false,
-      stream,
-      config: pc_config,
-    });
-
-    peer.on('signal', (signal) => {
-      socketRef.current.emit('returning signal', { signal, callerID });
-    });
-
-    peer.signal(incomingSignal);
-
-    return peer;
-  }
 
   return (
-    <Container>
-      <StyledVideo muted ref={userVideo} autoPlay playsInline />
-      {peers.map((peer: any, index: any) => {
-        return <Video key={index} peer={peer} />;
-      })}
-    </Container>
+    <Wrapper>
+      <ScreenWrapper>
+        {<ScreenView show={isTurnOnCamera} autoPlay ref={cameraRef} />}
+        {<ScreenView show={isShareScreen} autoPlay ref={displayRef} />}
+      </ScreenWrapper>
+
+      <ToolBar>
+        <ToolbarButton active={isTurnOnCamera} onClick={turnCamera}>
+          <CameraIcon />
+          Turn on camera
+        </ToolbarButton>
+
+        <ToolbarButton active={isShareScreen} onClick={shareScreen}>
+          <ScreenShareIcon />
+          Share screen
+        </ToolbarButton>
+
+        <Button onClick={() => modelRef.current?.toggle()} style={{ marginTop: 24 }} type="primary" size="large">
+          Start Stream
+        </Button>
+      </ToolBar>
+
+      <PostModal ref={modelRef} />
+    </Wrapper>
   );
 };
 
-export default Room;
+export default Stream;
