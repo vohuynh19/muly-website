7ebd2056207068a92f0f160e35c6757d94e7be9b
vhuynh19 <vohuynh01092002@gmail.com>
2023-01-11 09:00:16 +0700

    Done stream

--- views/Home/index.tsx
+++ views/Home/index.tsx
@@ -1,51 +1,51 @@
+import axiosInstance from '@src/apis/axios';
+import { ENDPOINTS } from '@src/apis/endpoints';
 import { uuid } from '@src/utils/functions/uuid';
+import { useQuery } from 'react-query';
 import Item from './Item';
 import { HorizontalList, Section, Text } from './styled';
 
-const mock = [
-  {
-    id: uuid(),
-    src: 'assets/images/video-thumbnail-3.jpeg',
-    title: 'Loc Phu Ho',
-    des: 'Loc phu ho xin chao tat ca cac ban',
+const defaultFilter = {
+  filter: {},
+  sort: {
+    createdAt: -1,
   },
-  {
-    id: uuid(),
-    src: 'assets/images/video-thumbnail-3.jpeg',
-    title: 'Loc Phu Ho',
-    des: 'Loc phu ho xin chao tat ca cac ban',
-  },
-  {
-    id: uuid(),
-    src: 'assets/images/video-thumbnail-3.jpeg',
-    title: 'Loc Phu Ho',
-    des: 'Loc phu ho xin chao tat ca cac ban',
-  },
-  {
-    id: uuid(),
-    src: 'assets/images/video-thumbnail-3.jpeg',
-    title: 'Loc Phu Ho',
-    des: 'Loc phu ho xin chao tat ca cac ban',
-  },
-  {
-    id: uuid(),
-    src: 'assets/images/video-thumbnail-3.jpeg',
-    title: 'Loc Phu Ho',
-    des: 'Loc phu ho xin chao tat ca cac ban',
-  },
-];
+  page: 1,
+  limit: 10,
+};
+
 const Home = () => {
+  const { data } = useQuery('stream-room/all', () => axiosInstance.post(ENDPOINTS.STREAM_ROOM.GET, defaultFilter));
+
+  if (!data?.data) {
+    return null;
+  }
+
   return (
     <div>
       <div style={{ padding: '16px 24px' }}>
-        <Item isBanner height="400px" width="100%" key={uuid()} {...mock[0]} />
+        <Item
+          isBanner
+          height="400px"
+          width="100%"
+          key={uuid()}
+          src={data?.data.docs[0].thumbnail || 'assets/images/video-thumbnail-3.jpeg'}
+          {...data?.data.docs[0]}
+        />
       </div>
 
       <Section>
         <Text>{'Hot Streamers'}</Text>
         <HorizontalList>
-          {mock.map((streamRoom) => (
-            <Item key={uuid()} {...streamRoom} />
+          {data?.data.docs.map((streamRoom: any) => (
+            <Item
+              key={streamRoom._id}
+              id={streamRoom._id}
+              isBanner
+              src={streamRoom.thumbnail || 'assets/images/video-thumbnail-3.jpeg'}
+              title={streamRoom.title}
+              des={streamRoom.des}
+            />
           ))}
         </HorizontalList>
       </Section>
